8c28eb5817773b8dcab0e4182d7ef025d247a0ec
Tanushree Tunstall <tanu@tunstall.in>
2024-11-05 18:22:19 +0000

    extracting uniq entries

--- processing.py
+++ processing.py
@@ -102,61 +102,12 @@ pp.pprint(list(proteomeD.items())[:5])
 # Stage 2: assign proteome id to protein names
 #find_proteome_id()
 #============
-# # Start the timer
-# start_time = time.time()
-
-# print("\nReading protein cluster file:", protein_cluster_infile)
-# #protein_cluster_file = pd.read_csv(protein_cluster_infile, sep='\t', header=None)
-
-# # tsv file has no headers, so giving it names
-# pclustersDF = pd.read_csv(protein_cluster_infile
-#                           , sep ='\t'
-#                           , usecols = [0,1], names=['protein1_id', 'protein2_id'])
-
-# print(pclustersDF.shape)
-# #TODO: room for optimisation
-# #pclustersDF['protein1_id'].value_counts() # may be some optimisation can be done here!
-# #pclustersDF['protein2_id'].value_counts()
-# print("\n No. of unique protein_ids in the two columns:\n", pclustersDF.nunique())
-# check1 = pclustersDF.groupby('protein1_id').count()
-
-# pclustersDF['protein1_id'].nunique()
-# pclustersDF['protein1_id'].count()
-# pclustersDF['protein1_id'].size
-# pclustersDF['protein1_id'].value_counts()
-
-# # Start timing
-# #start_time = time.time()
-
-# # Apply the function to `protein2_id` column with progress tracking
-# tqdm.pandas(desc="Mapping protein2_id to proteome ID")
-
-# ## Adding proteome id to columns: 
-
-# # FIXME: Adding to column: 'protein1_id'
-# pclustersDF['protein1_id_proteome'] = pclustersDF['protein1_id'].progress_apply(
-#     lambda name: f"{find_proteome_id(name, proteomeD)}_{name}"
-# )
-
-# # Adding to column: 'protein2_id'
-# pclustersDF['protein2_id_proteome'] = pclustersDF['protein2_id'].progress_apply(
-#     lambda name: f"{find_proteome_id(name, proteomeD)}_{name}"
-# )
-
-# # End timing
-# end_time = time.time()
-# elapsed_time_s2 = end_time - start_time
-
-# # Output results
-# print(f"Mapping completed in {elapsed_time_s2 // 3600:.0f} hours, "
-#       f"{(elapsed_time_s2 % 3600) // 60:.0f} minutes, and {elapsed_time_s2 % 60:.2f} seconds.")
-# print("Sample mapped entries:", pclustersDF[['protein2_id', 'protein2_id_proteome']].head())
-
-
-###############################################################################
 
 # Read in the TSV file with protein pairs
-pclustersDF = pd.read_csv(protein_cluster_infile, sep='\t', usecols=[0, 1], names=['protein1_id', 'protein2_id'])
+pclustersDF_original = pd.read_csv(protein_cluster_infile, sep='\t', usecols=[0, 1], names=['protein1_id', 'protein2_id'])
+
+pclustersDF = pclustersDF_original.drop_duplicates()
+print(f"\nLength of DF of unique entries:{len(pclustersDF)}")
 
 # Start timing
 start_time = time.time()
@@ -174,13 +125,10 @@ pclustersDF['protein2_id_proteome'] = pclustersDF['protein2_id'].progress_apply(
     #lambda name: f"{find_proteome_id(name, proteomeD)}_{name}")
     lambda name: f"{find_proteome_id(name, proteomeD)}")
 
-
 # # without function:
 # pclustersDF['protein2_id_proteome'] = pclustersDF['protein2_id'].progress_apply(
-#     lambda name: f"{proteomeD.get(name, 'XXX')}_{name}"
+#     lambda name: f"{proteomeD.get(name, 'XXX')}_{name}")
 #    lambda name: f"{find_proteome_id(name, proteomeD)}")
-
-# )
 
 # Sort the DataFrame by 'cluster_id' in ascending order without resetting the index
 pclustersDF = pclustersDF.sort_values(by='cluster_id')
@@ -194,6 +142,32 @@ print(f"Mapping completed in {elapsed_time_s2 // 3600:.0f} hours, "
       f"{(elapsed_time_s2 % 3600) // 60:.0f} minutes, and {elapsed_time_s2 % 60:.2f} seconds.")
 print("Sample mapped entries:", pclustersDF[['cluster_id', 'protein1_id', 'protein2_id', 'representative', 'protein2_id_proteome']].head())
 
+
+#a  = pclustersDF.iloc[[680512,680513]]
+
+b = pclustersDF[(pclustersDF['cluster_id'] == 301) & (pclustersDF['protein2_id_proteome'].str.contains("ENSSSCP00040031975"))]
+
+pclustersDF.shape
+
+fd_diff = pclustersDF.shape[0] - len(proteomeD)
+print(f"\nDiff between tsv file and dict length: {fd_diff} \
+      \nLength of TSV file: {pclustersDF.shape[0]} \
+      \nLength of proteomeD: {len(proteomeD)}")
+
+
+duplicateRowsDF = pclustersDF[pclustersDF.duplicated()]
+print(f"\nDuplicated rows in pclustersDF: {duplicateRowsDF.shape[0]}")
+
+predicted_nunique_entries = pclustersDF.shape[0] - duplicateRowsDF.shape[0]
+print(f"\nPredicted no. of unique entries:{predicted_nunique_entries}")
+
+pclustersDF_unique = pclustersDF.drop_duplicates()
+print(f"\nLength of DF of unique entries:{len(pclustersDF_unique)}")
+
+#TODO
+# add sanity check here
+
+
 ##############################################################################
 #=========================
 # writing the updated file
@@ -202,7 +176,7 @@ print("Sample mapped entries:", pclustersDF[['cluster_id', 'protein1_id', 'prote
 output_clusterDF_file = results_indir + '/species_protein_cluster_with_proteomes_v3.tsv'
 cols_to_output = ['cluster_id', 
                   #'protein1_id', 
-                  #'protein2_id', 
+                  'protein2_id', 
                   'representative', 
                   'protein2_id_proteome']
 
