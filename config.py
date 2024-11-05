4f06b3bdfb30e690a66aa6866a54ac4f021d9da4
Tanushree Tunstall <tanu@tunstall.in>
2024-11-05 19:39:17 +0000

    removed old hardcoded filenames

--- config.py
+++ config.py
@@ -12,35 +12,6 @@ Created on Thu Oct 31 12:15:23 2024
 #from glob import glob
 #from tqdm import tqdm
 #from Bio import SeqIO # pip install biopython
-
-###############################################################################
-#==================
-# source dir
-#==================
-# # ebi-cluster
-# # = "/nfs/production/martin/uniprot/users/insana/proteomescomparison"
-
-# # home
-# source_dir = "/home/pub/Work/data_arise_proteome"
-
-# # ebi-local machine
-# #source_dir = "/home/tunstall/Documents/arise/data_arise_proteome"
-
-# #==================
-# # files
-# #==================
-# # pig proteome file
-
-# ## all .fa files
-# proteome_indir = source_dir + "/pig"
-# print("\nReading proteome file from:", proteome_indir)
-
-# ## results files: .tsv
-# results_indir = source_dir + "/results_pig"
-# protein_cluster_infile = results_indir + "/Specie_protein_cluster.tsv" 
-# #protein_cluster_infile = results_indir + "/sample_Specie_protein_cluster.tsv"
-
-# #outdir = results_indir
 ###############################################################################
 import argparse
 import os
