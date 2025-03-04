496b583a2874cb6aabdb5f214653fd864c314bc7
Tanushree Tunstall <tanu@tunstall.in>
2025-03-04 18:30:40 +0000

    checking and checking and more checking

--- exclusion_processing.py
+++ exclusion_processing.py
@@ -21,8 +21,8 @@ import numpy as np
 ######
 # Read data
 homedir = os.path.expanduser("~")
-basedir =  homedir + "/Documents/arise/spneumo_dataset"
-#basedir =  "/home/pub/Work/data_arise_proteome/spneumo_dataset"
+#basedir =  homedir + "/Documents/arise/spneumo_dataset"
+basedir =  "/home/pub/Work/data_arise_proteome/spneumo_dataset"
 
 # Load TSV files
 up_spneumo_proteomes = basedir + "/spneumo_biosample_info.out"
@@ -37,95 +37,164 @@ exclusion_reasons = basedir + "/exclusion_reasons.tsv"
 #df_up_all = df_up_original[df_up_original['protein_count']> 0]
 #print(f"\nNo. of of entries with 0 proteins: {len(df_up_original[df_up_original['protein_count']== 0])}")
 #print(f"\nLength of UP DF after removing proteomes with 0 proteins: {len(df_up_all)}")
-   
-
 
 ###############################################################################
-def merge_tsv_files(file1_path, file2_path, 
+import pandas as pd
+
+def merge_data(file1, file2, 
                     file1_merge_col='exclusion_id', 
                     file2_merge_col='id', 
                     include_columns="All",
-                    join_type='left'):
+                    join_type='left',
+                    delimiter = "\t",
+                    drop_cols=[]):
     """
-    Merges two TSV files based on a merge column. Allows for different column names
-    in each file for the merge key and includes specified columns from the second file or all by default.
+    Merges two datasets based on a merge column. Allows for different column names
+    in each dataset for the merge key and includes specified columns from the second dataset or all by default.
     The type of join performed can be specified to control how rows from each dataset are included in the result.
+    Each dataset can either be a DataFrame or a file path to a TSV file.
 
     Args:
-    - file1_path (str): Path to the first TSV file.
-    - file2_path (str): Path to the second TSV file, containing additional data like exclusion meanings.
-    - file1_merge_col (str): Column name in the first file to merge on. Default is 'exclusion_id'.
-    - file2_merge_col (str): Column name in the second file to merge on. Default is 'id'.
-    - include_columns (str or list): Specific columns from the second file to add to the first. If "All", all columns are added.
+    - file1 (str or pd.DataFrame): First dataset or path to the TSV file.
+    - file2 (str or pd.DataFrame): Second dataset or path to the TSV file, containing additional data like exclusion meanings.
+    - file1_merge_col (str): Column name in the first dataset to merge on. Default is 'exclusion_id'.
+    - file2_merge_col (str): Column name in the second dataset to merge on. Default is 'id'.
+    - include_columns (str or list): Specific columns from the second dataset to add to the first. If "All", all columns are added.
     - join_type (str): Type of join to perform during the merge. Options include 'left', 'right', 'outer', 'inner'. Default is 'left'.
 
     Returns:
     - pd.DataFrame: A DataFrame with the merged content.
 
     Raises:
-    - ValueError: If any specified columns in `include_columns` do not exist in the second file.
+    - ValueError: If any specified columns in `include_columns` do not exist in the second dataset.
     """
-    
-    # Load the data files
-    df1 = pd.read_csv(file1_path, sep='\t')
-    df2 = pd.read_csv(file2_path, sep='\t')
 
+    # Load or use data frames
+    if isinstance(file1, str):
+        df1 = pd.read_csv(file1, sep=delimiter)
+    elif isinstance(file1, pd.DataFrame):
+        df1 = file1
+    else:
+        raise ValueError("file1 must be a filepath or a DataFrame")
+
+    if isinstance(file2, str):
+        df2 = pd.read_csv(file2, sep=delimiter)
+    elif isinstance(file2, pd.DataFrame):
+        df2 = file2
+    else:
+        raise ValueError("file2 must be a filepath or a DataFrame")
+        
     # Rename the merge column in file2 if necessary
-    if file2_merge_col != file1_merge_col:
-        df2.rename(columns={file2_merge_col: file1_merge_col}, inplace=True)
+    #if file2_merge_col != file1_merge_col:
+    #    df2.rename(columns={file2_merge_col: file1_merge_col}, inplace=True)
 
     # Check if specific columns are to be included from the second file
     if include_columns != "All":
         if not set(include_columns).issubset(df2.columns):
             missing_cols = set(include_columns) - set(df2.columns)
-            raise ValueError(f"The following columns are specified in include_columns but do not exist in second file {missing_cols}")
+            raise ValueError(f"The following columns are specified in include_columns but do not exist in the second dataset: {missing_cols}")
         columns_to_include = [file1_merge_col] + include_columns
         df2 = df2[columns_to_include]
 
     # Merge the dataframes on the specified column
-    merged_df = pd.merge(df1, df2, on=file1_merge_col, how=join_type)
+    #merged_df = pd.merge(df1, df2, on=file1_merge_col, how=join_type)
+    merged_df = pd.merge(df1, df2, left_on=file1_merge_col, right_on=file2_merge_col, how=join_type)
+    
+    # Drop specified columns if drop_cols is not None or empty
+    if drop_cols and len(drop_cols) > 0:
+        merged_df = merged_df.drop(drop_cols, axis=1)
 
     return merged_df
+
 ###############################################################################
 # Example usage:
 # Merging and including all columns from the second file (default behavior)
+merged_data_all = merge_data(file1=up_spneumo_proteomes, 
+                                  file2=exclusion_reasons,
+                                  file1_merge_col='exclusion_id', 
+                                  file2_merge_col='id', 
+                                  include_columns="All",
+                                  join_type='left',
+                                  delimiter ='\t',
+                                  drop_cols = ['id'])
 
-merged_data_all = merge_tsv_files(file1_path=up_spneumo_proteomes, 
-                                  file2_path=exclusion_reasons,
-                                  include_columns="All")
-a = merged_data_all[merged_data_all['gc_set_acc'].isin(['GCA_001255215.2', 'GCA_001255215.1'])]
-# Merging and specifying only certain columns to include from the second file
-merged_data_specific = merge_tsv_files(file1_path=up_spneumo_proteomes, 
-                                  file2_path=exclusion_reasons,
-                                  include_columns=['id_description'])
+upid_nunique_source = merged_data_all['upid'].nunique()
+print(f"\nLength of source data: {len(merged_data_all)}")
+print(f"Number of unique upids in source data: {upid_nunique_source}")
 
-print("Merged with all columns (default):")
 print(merged_data_all.head())
+
+
+#a = merged_data_all[merged_data_all['gc_set_acc'].isin(['GCA_001255215.2', 'GCA_001255215.1'])]
+# Merging and specifying only certain columns to include from the second file
+#merged_data_specific = merge_tsv_files(file1_path=up_spneumo_proteomes, 
+#                                  file2_path=exclusion_reasons,
+#                                  include_columns=['id_description'])
 
 #print("\nMerged with specific columns:")
 #print(merged_data_specific.head())
 
 ###############################################################################
-# checkM
-checkM_ncbi = pd.read_csv(basedir + "/CheckM_report_prokaryotes.txt", sep = "\t")
+# checkM: See next!
+# FIXME: later!
+checkM_filepath = basedir + "/CheckM_report_prokaryotes.txt"
+#checkM_ncbi = pd.read_csv(basedir + "/CheckM_report_prokaryotes.txt", sep = "\t")
+checkM_ncbi = pd.read_csv(checkM_filepath, sep = "\t")
 upcheckM1= merged_data_all[merged_data_all['gc_set_acc'].isin(checkM_ncbi['#genbank-accession'])]
 upNOTcheckM1 = merged_data_all[~merged_data_all['gc_set_acc'].isin(checkM_ncbi['#genbank-accession'])]
 
+checkM_ncbi.columns
 
-checkM_sp = pd.read_csv(basedir + "/CheckM_report_streptococcus_pneumoniae.txt", sep = "\t")
-upcheckM = merged_data_all[merged_data_all['gc_set_acc'].isin(checkM_sp['#genbank-accession'])]
-upNOTcheckM = merged_data_all[~merged_data_all['gc_set_acc'].isin(checkM_sp['#genbank-accession'])]
+checkM_ncbi['gc_set_acc']
+checkM_ncbi['#genbank-accession']
+
+df_with_checkM = merge_data(file1=merged_data_all,
+                                file2=checkM_ncbi,
+                                file1_merge_col="gc_set_acc", 
+                                file2_merge_col="#genbank-accession", 
+                                include_columns=['#genbank-accession', 'refseq-accession', 'checkm-completeness','checkm-contamination'],
+                                join_type='left',
+                                delimiter = '\t')
+
+df_with_checkM['species_taxid'].value_counts()
+df_with_checkM['species-taxid'].value_counts()
+
+df_with_checkM['proteome_taxid'].value_counts()
+df_with_checkM['taxid'].value_counts()
 
 
-(upcheckM1 == upcheckM).all()
-(upNOTcheckM1 == upNOTcheckM).all()
+# 
+# Drop rows where either 'species_taxid' or 'species-taxid' is NaN
+small_df = df_with_checkM.dropna(subset=['species_taxid', 'species-taxid', 'proteome_taxid', 'taxid'])
+
+# Perform the comparison and create a new column with the result
+small_df['comparison_result'] = small_df['species_taxid'] == small_df['species-taxid']
+small_df['comparison_result2'] = small_df['proteome_taxid'] == small_df['taxid']
+
+all_equal = small_df['comparison_result'].all()
+print("All non-NaN values are equal:", all_equal)
+
+all_equal2 = small_df['comparison_result2'].all()
+print("All non-NaN values are equal:", all_equal2)
+
+
+print(f"\nLength of checkM_ncbi: {len(checkM_ncbi)}")
+
+print(f"\nLength of available checkM for spneumo acc to GA: {len(upcheckM1)}")
+print(f"\nTotal unique UP proteomes with available checkM according to GA: {upcheckM1['upid'].nunique()}") #10161
+
+print(f"\nLength of unavailable checkM for spneumo acc to GA: {len(upNOTcheckM1)}")
+print(f"\nTotal unique UP proteomes with UNavailable checkM according to GA: {upNOTcheckM1['upid'].nunique()}") #16588
+
+#checkM_sp = pd.read_csv(basedir + "/CheckM_report_streptococcus_pneumoniae.txt", sep = "\t")
+#upcheckM = merged_data_all[merged_data_all['gc_set_acc'].isin(checkM_sp['#genbank-accession'])]
+#upNOTcheckM = merged_data_all[~merged_data_all['gc_set_acc'].isin(checkM_sp['#genbank-accession'])]
+
+#(upcheckM1 == upcheckM).all()
+#(upNOTcheckM1 == upNOTcheckM).all()
+
 #################
 # code for is_effective == t and exclusion_ids 1,7,9,14,29,94,96,99
-
-# Usage
-ids_to_keep = [29, 94, 96, 99]
-#ids_to_omit = [1, 2, 7, 9, 14, 26] #26514
-ids_to_omit = [1, 7, 9, 14, 2] #26546
 
 assembly_level_to_exclude = ['partial']
 excluded_protein_counts = [0]
@@ -188,21 +257,36 @@ def move_cols_to_end(df, cols):
 
 ############
 df = merged_data_all.copy()
+df_unique = df.drop_duplicates(['upid', 'protein_count'])
+
 print(f"\nLength of input data: {len(df)}")
 print(f"\nLength of input data unique proteomes: {df['upid'].nunique()}")
 print(f"\nCount effective exclusions:\n {df['is_effective'].value_counts()}")
+print(f"\nUnique Count effective exclusions:\n {df_unique['is_effective'].value_counts()}")
 
 print(f"\nExcluding proteomes with protein count: {excluded_protein_counts}")
-df2 = df[~df['protein_count'].isin(excluded_protein_counts)]
-print(f"\nLength of filtered data unique proteomes: {df2['upid'].nunique()}")
-print(f"\nLength of df: {len(df2)}")
-print(f"\nNo of unique proteomes removed: {df['upid'].nunique() - df2['upid'].nunique()}")
+df1 = df[~df['protein_count'].isin(excluded_protein_counts)]
+df1_unique = df_unique[~df_unique['protein_count'].isin(excluded_protein_counts)]
+
+print(f"\nLength of unique proteomes in filtered data: {df1['upid'].nunique()}")
+print(f"\nLength of df: {len(df1)}")
+print(f"\nNo of unique proteomes removed: {df['upid'].nunique() - df1['upid'].nunique()}")
+print(f"\nSanity check2 : No of unique proteomes removed: {len(df_unique) - len(df1_unique)}")
+a = df_unique['protein_count'].value_counts()
+a[0] #76 should match this
 
 print(f"\nExcluding proteomes with assembly level: {assembly_level_to_exclude}")
-df2 = df2[~df2['assembly_level'].isin(assembly_level_to_exclude)]
-print(f"\nLength of df: {len(df2)}")
+df2 = df1[~df1['assembly_level'].isin(assembly_level_to_exclude)]
+df2_unique =  df1_unique[~df1_unique['assembly_level'].isin(assembly_level_to_exclude)]
 
-#3 upids belongig to 10 records have partial
+print(f"\nLength of df: {len(df2)}")
+print(f"\nNo of unique proteomes removed: {df1['upid'].nunique() - df2['upid'].nunique()}")
+print(f"\nSanity check2 : No of unique proteomes removed: {len(df1_unique) - len(df2_unique)}")
+a2 = df1_unique['assembly_level'].value_counts()
+a2['partial']
+
+
+#4 upids belongig to 10 records have partial
 #	upid
 #3944	UP000243639
 #3945	UP000243639
@@ -217,30 +301,39 @@ print(f"\nLength of df: {len(df2)}")
 
 print(f"\nCount of effective exclusions with 'protein_count > 0' and 'assembly level not partial':\n {df2['is_effective'].value_counts()}")
 
+# Usage
 ids_to_keep = [29, 94, 96, 99]
-#ids_to_omit = [1, 2, 7, 9, 14, 26] #26514
-ids_to_omit = [1, 7, 9, 14, 2] 
-ids_to_omit = [1, 7, 9, 14] 
+ids_to_omit = [1, 7, 9, 14, 2]    #26546
+
 
 # Step 1: Identify upids to remove
 grouped = df2.groupby('upid')['exclusion_id'].agg(set)
 #grouped2 = df2.groupby('upid')['exclusion_id'].agg(list)
+print(f"\nLength of unique proteomes in grouped: {len(grouped)}")
+
+df2.groupby('upid').nunique() # should match
 
 upids_to_remove = grouped[
     grouped.apply(lambda ids: any(i in ids_to_omit for i in ids) and not any(i in ids_to_keep for i in ids))
 ].index
 
-upids_to_filter = grouped[
-    grouped.apply(lambda ids: any(i in ids_to_keep for i in ids) and any(i in ids_to_omit for i in ids))
-].index
+if len(ids_to_keep) > 0: 
+    upids_to_filter = grouped[
+        grouped.apply(lambda ids: any(i in ids_to_keep for i in ids) and any(i in ids_to_omit for i in ids))
+    ].index
+
+print(f"\nLength of unique proteomes to remove: {(upids_to_remove).nunique()}")
+print(f"\nLength of unique proteomes to filter: {upids_to_filter.nunique()}")
+#print(f"\nLength of Total unique proteomes to : {upids_to_filter.nunique()}")
 
 
-upids_to_remove = grouped[
+upids_to_remove1 = grouped[
     grouped.apply(lambda ids: any(i in ids_to_omit for i in ids))].index
+print(f"\nLength of unique proteomes to remove with second criteria: {(upids_to_remove1).nunique()}")
 
 
 a = upids_to_remove.to_list() + upids_to_filter.to_list()
-
+len(a)
 check2 =df2[~df2['upid'].isin(upids_to_remove.to_list() + upids_to_filter.to_list())]
 #some upids to check: UP000000685, UP000002642, UP000235432 (8772)
 
